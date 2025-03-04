544a961713b95d3804a7658fdb76385eae4349bd
Blank <wangfj11@foxmail.com>
2025-03-04 20:58:21 +0800

    [同步]1. 同步主库到2024年8月25日   0091922cf1f116c9c780ef29e373e35664341539
    fixed a bug handling empty fragment incorrectly

--- src/GameFrameX.SuperSocket.WebSocket/FramePartReader/PackagePartReader.cs
+++ src/GameFrameX.SuperSocket.WebSocket/FramePartReader/PackagePartReader.cs
@@ -17,27 +17,27 @@ namespace GameFrameX.SuperSocket.WebSocket.FramePartReader
         {
             get { return FixPartReader; }
         }
-    
+
         /// <summary>
         /// 固定部分读取器
         /// </summary>
         protected static IPackagePartReader<WebSocketPackage> FixPartReader { get; private set; }
-    
+
         /// <summary>
         /// 扩展长度读取器
         /// </summary>
         protected static IPackagePartReader<WebSocketPackage> ExtendedLengthReader { get; private set; }
-    
+
         /// <summary>
         /// 掩码密钥读取器
         /// </summary>
         protected static IPackagePartReader<WebSocketPackage> MaskKeyReader { get; private set; }
-    
+
         /// <summary>
         /// 负载数据读取器
         /// </summary>
         protected static IPackagePartReader<WebSocketPackage> PayloadDataReader { get; private set; }
-    
+
         static PackagePartReader()
         {
             FixPartReader = new FixPartReader();
@@ -45,17 +45,27 @@ namespace GameFrameX.SuperSocket.WebSocket.FramePartReader
             MaskKeyReader = new MaskKeyReader();
             PayloadDataReader = new PayloadDataReader();
         }
-    
+
         public abstract bool Process(WebSocketPackage package, object filterContext, ref SequenceReader<byte> reader, out IPackagePartReader<WebSocketPackage> nextPartReader, out bool needMoreData);
-    
+
         protected bool TryInitIfEmptyMessage(WebSocketPackage package)
         {
             if (package.PayloadLength != 0)
+            {
                 return false;
-    
+            }
+
+            // This fragment is empty doesn't mean the whole message is empty
+            if (package.Head != null)
+            {
+                return false;
+            }
+
             if (package.OpCode == OpCode.Text)
+            {
                 package.Message = string.Empty;
-    
+            }
+
             return true;
         }
     }
